ac3c97fd93161662a73edd5f41dac1bb0bfc977e
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-02-24 12:33:08 +0100

    Initial Commit

---
+++ Volumetric clock/Volumetric_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volumetric_clockApp.swift
+//  Volumetric clock
+//
+//  Created by Pavel Krigin on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Volumetric_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
